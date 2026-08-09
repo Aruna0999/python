9a9f7f7e4f69fef4091bc2102cb7a2c979051ae0
Aruna0999 <aruna319355@gmail.com>
2026-08-09 22:09:27 +0530

    Add files via upload

---
+++ map.py
@@ -0,0 +1,65 @@
+#map()->each object from a collection and forms a new collection
+
+#max(),min(),sum()
+
+'''print(max(4,5,7,8,10,20))
+
+print(min(4,5,7,8,10,20))
+
+a=3,4,5,6,7
+print(sum(a))'''
+
+#map()
+'''a=[2,5,7,8,10,12,14,16,20,25,30]
+b=[1,3,5,7,9,11,15,17,21,24,30]
+c=list(map(max,a,b))
+print(c)
+c=list(map(min,a,b))
+print(c)'''
+
+'''a=input("data1")
+b=input("data2")
+print(a+b)'''
+
+'''a,b=input("enter the names").split(",")
+print(a+b)'''
+
+'''a,b=[x for x in input("names").split(",")]
+print(a+b)'''
+
+'''a,b=map(str,input("enter the names").split(","))
+print(a+b)'''
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+'''a,b=[int(x) for x in input().split(",")]
+print(a+b)'''
+
+'''a,b=int(input()).split(",")
+print(a+b)'''#error
+
+'''a,b=map(int,input("enter the values").split(","))
+print(a+b)'''
+
+'''a=list(map(int,input("enter the values").split(",")))
+print(a)
+print(type(a))'''
+
+'''a=tuple(map(eval,input("enter the values").split(",")))
+print(a)'''
+
+'''a=set(map(eval,input("enter the values").split(",")))
+print(a)'''
+
+'''a=input("enter the key and value pairs")
+b=dict(i.split(":") for i in a.split(","))
+print(b)'''
+
+
+
+
+
+
+
